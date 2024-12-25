e67d5507212a18d3205513da1e17b24d4e9fdda8
Emon <atitudemon@gmail.com>
2024-12-25 23:28:18 +0600

    Triangle area program

--- Math/triangle.c
+++ Math/triangle.c
@@ -5,13 +5,13 @@ int main() {
 
     double a, b, c, s, area;
 
-    printf("Enter 3 values: ");
+    printf("Enter 3 bars value of triangle: ");
     scanf("%lf %lf %lf", &a, &b, &c);
 
     s = (a + b + c)/2;
     area = sqrt(s*(s-a)*(s-b)*(s-c));
 
-    printf("Area = %.2lf", area);
+    printf("Area of your triangle = %.2lf", area);
 
     return 0;
 }
