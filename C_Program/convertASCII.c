a640dca58676eee2bb076c06087a80d6ebc4ceea
Emon <atitudemon@gmail.com>
2024-12-30 16:39:38 +0600

    ascii

---
+++ C_Program/convertASCII.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    // ASCII value of a character, Number, Special character
+    char character;
+    printf("Enter any character: ");
+    character = getchar();
+    printf("ASCII value of %c = %d\n", character, character);
+
+    
+
+    return 0;
+}
