764057341416f0e30da21e07e35e026c791c1857
Emon <atitudemon@gmail.com>
2024-12-30 17:07:10 +0600

    ascii

--- C_Program/convertASCII.c
+++ C_Program/convertASCII.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 
 int main() {
-    // ASCII value of a character, Number, Special character
+    // ASCII value of a number and character
+    int num;
     char character;
-    printf("Enter any character: ");
-    character = getchar();
-    printf("ASCII value of %c = %d\n", character, character);
 
-    
+    // ASCII value of a number
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    printf("ASCII value of %d = %c\n", num, num);
+
+    // ASCII value of a character
+    // printf("Enter any character: ");
+    // character = getchar();
+    character =  num;
+    printf("ASCII value of %c = %d\n", character, character);
 
     return 0;
 }
