d6ad3703f7ba32a29088a5e43ecac84360169b16
Emon <atitudemon@gmail.com>
2024-12-31 00:04:57 +0600

    ascii

---
+++ C_Program/lowerupper.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main() {
+    // Convert lowercase and uppercase character
+    char character, lowercase, uppercase;
+    printf("Enter any character: ");
+    character = getchar();
+
+    printf("ASCII decimal value of %c = %d\n\n", character, character);
+
+    uppercase = character - 32; // toupper(character);
+    lowercase = character + 32; //tolower(character);
+
+    if (character >= 'a' && character <= 'z') {
+        printf("Uppercase character of %c = %c\n", character, uppercase);
+    }else if (character >= 'A' && character <= 'Z') {
+        printf("Lowercase character of %c = %c.\n", character, lowercase);}
+    else {
+        printf("%c is Invalid character.\n", character);
+    }
+
+    return 0;
+}
