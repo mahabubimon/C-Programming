2620447362bd128fea2c44517bab32676e34ea9a
Emon <atitudemon@gmail.com>
2025-01-02 17:33:05 +0600

    reversed update

--- Loop/reversenum.c
+++ Loop/reversenum.c
@@ -2,18 +2,28 @@
 
 int main() {
     // Program to reverse a number
-    int num, rev = 0;
+    int num1, num2, rev1 = 0, rev2 =0;
 
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    // Reversing the number using for loop
+    printf("Enter 1st number: ");
+    scanf("%d", &num1);
 
-    // Reversing the number
-    while(num) {
-        rev = rev * 10 + num % 10;
-        num = num / 10;
+    for(num1; num1 != 0; num1 /= 10) {
+        rev1 = rev1 * 10 + num1 % 10;
     }
 
-    printf("Reversed number: %d\n", rev);
+    printf("Reversed number: %d\n", rev1);
+
+    // Reversing the number using while loop
+    printf("Enter 2nd number: ");
+    scanf("%d", &num2);
+
+    while(num2) {
+        rev2 = rev2 * 10 + num2 % 10;
+        num2 = num2 / 10;
+    }
+
+    printf("Reversed number: %d\n", rev2);
 
     return 0;
 }
