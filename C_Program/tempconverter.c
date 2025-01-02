ce68405451159e433e9104bd17b3d881f5e76d82
Emon <atitudemon@gmail.com>
2025-01-02 22:05:53 +0600

    temp converter

--- C_Program/tempconverter.c
+++ C_Program/tempconverter.c
@@ -1,23 +1,43 @@
 #include<stdio.h>
 
 int main() {
-    // Celsius to Fahrenheit
-    float tempC1, tempF1, tempC2, tempF2;
-    printf("Enter temp in Celsius: ");
-    scanf("%f", &tempC1);
+    // Temperature Converter
+    int choice;
+    float temp;
+
+    printf("Celsius to Fahrenheit: 1 \nFahrenheit to Celsius: 2\n");
+    printf("Enter your temp converter choice 1 or 2: \n");
+    scanf("%d", &choice);
     
-    // Formula: F = (C * 1.8) + 32
-    tempF1 = (tempC1 * 1.8) + 32;
+    switch (choice)
+    {
+        case 1:
+        {
+            // Celsius to Fahrenheit
+            printf("Enter temperature in Celsius: ");
+            scanf("%f", &temp);
 
-    printf("Temp in Fahrenheit = %.2f\n\n", tempF1);
+            // Formula: F = (C * 1.8) + 32
+            temp = (temp * 1.8) + 32;
+            printf("Temp in Fahrenheit = %.2f\n", temp);
+        }
+        break;
 
-    // Fahrenheit to Celsius
-    printf("Enter temp in Fahrenheit: ");
-    scanf("%f", &tempF2);
+        case 2: 
+        { 
+            // Fahrenheit to Celsius
+            printf("Enter temperature in Fahrenheit: ");
+            scanf("%f", &temp);
 
-    // Formula: C = (F - 32) / 1.8
-    tempC2 = (tempF2 - 32) / 1.8;
-    printf("Temp in Celsius = %.2f\n", tempC2);
+            // Formula: C = (F - 32) / 1.8 
+            temp = (temp - 32) / 1.8;
+            printf("Temp in Fahrenheit = %.2f\n", temp);;
+        }
+        break;
+    
+        default: printf("Invalid choice.\n");
+        break;
+    }
 
     return 0;
 }
