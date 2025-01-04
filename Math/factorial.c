abc1212cb58b7ca6a0ba07cb4b3b3a8ab6a5830d
Emon <atitudemon@gmail.com>
2025-01-04 17:08:30 +0600

    factorial of a number

---
+++ Math/factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+    // Factorial of a number
+    while (1) {
+        int i, num;
+        int factorial = 1;
+
+        printf("\nEnter any positive number: ");
+        scanf("%d", &num);
+
+        for ( i = 1; i <= num; i++) {
+        factorial *= i;
+        }
+     
+        printf("Factorial of %d: %d\n", num, factorial);
+    }
+
+    return 0;
+}
