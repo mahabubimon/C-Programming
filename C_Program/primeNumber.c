95dbbe9e93d4ebd97960edca468897e6350b851d
Emon <atitudemon@gmail.com>
2025-01-05 23:11:41 +0600

    count and sum of prime numbers between two numbers

--- C_Program/primeNumber.c
+++ C_Program/primeNumber.c
@@ -1,28 +1,49 @@
 #include<stdio.h>
+#include<math.h>
 
 int main() {
+    // Count, print and sum of prime numbers between two numbers
 
-    // Prime number
-    int num, i, count = 0;
+    int startingNum, endingNum, countPrimeNumbers = 0, sumPrimeNumbers = 0, i, j, f=2;
 
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    printf("Count, print and sum of prime numbers between two numbers.\n");
 
-    for (i = 2; i <= num / 2; i++) {
-        if (num % i == 0) {
-            count++;
-            break;
+    printf("Enter starting number: ");
+    scanf("%d", &startingNum);
+
+    printf("Enter ending number: ");
+    scanf("%d", &endingNum);
+
+    // Check if starting number is less than ending number
+
+    if (startingNum < endingNum && startingNum > 0 && endingNum > 1) {
+        // Loop through the numbers to find prime numbers
+
+        printf("Prime numbers between %d and %d are: ", startingNum, endingNum);
+
+        for (i = startingNum; i <= endingNum; i++) {
+
+            int count = 0;
+
+            for (j = 2; j <= sqrt(i); j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0 && i > 1) {
+                printf("%d, ", i);
+                countPrimeNumbers++;
+                sumPrimeNumbers += i;
+            }
         }
-    }
 
-    if (num <= 1) {
-        printf("%d is neither prime nor composite.\n", num);
+        printf("\nTotal prime numbers between %d and %d are: %d\n", startingNum, endingNum, countPrimeNumbers);
+        printf("Sum of prime numbers between %d and %d is: %d\n", startingNum, endingNum, sumPrimeNumbers);
+
     } else {
-        if (count == 0) {
-            printf("%d is a prime number.\n", num);
-        } else {
-            printf("%d is not a prime number.\n", num);
-        }
+        printf("Invalid input!!! Enter ending number greater than 1. ");
+        printf("Starting number should be less than ending number!!!\n");
     }
 
     return 0;
