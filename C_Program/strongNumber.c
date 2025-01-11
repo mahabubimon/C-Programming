928ff5aa114657e6bea544eb783795cfcb896cbb
Emon <atitudemon@gmail.com>
2025-01-11 14:02:20 +0600

    find strong number program

---
+++ C_Program/strongNumber.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main() {
+
+    int num, temp, rem, fact, sum = 0, i;
+
+    printf("Enter an integer number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while (temp != 0) {
+        rem = temp % 10;
+        
+        fact = 1;
+        for (i = 1; i <= rem; i++) {
+            fact = fact * i;
+        }
+        sum = sum + fact;
+
+        temp = temp / 10;
+    }
+
+    if (num == sum) {
+        printf("%d is a STRONG number.\n", num);
+    } else {
+        printf("%d is not a STRONG number.\n", num);
+    }
+
+    return 0;
+}
