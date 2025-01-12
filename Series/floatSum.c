565abb3d2615e566abcec2591c2bc6004b684edf
Emon <atitudemon@gmail.com>
2025-01-12 22:19:10 +0600

    series

---
+++ Series/floatSum.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+
+    int num, i;
+    double sum = 0;
+
+    printf("Series: 1 + 1/2 + 1/3 .... + 1/n.\nEnter n = ");
+    scanf("%d", &num);
+
+    for (i=1; i<=num; i++) {
+        sum = sum + 1/(double)i;
+
+        if(i==1) {
+            printf("1 + ");
+        } else if (i==num) {
+            printf("1/%d", num);
+        } else {
+            printf("1/%d + ", i);
+        }
+    }
+
+    printf(" = %.2lf", sum);
+
+    return 0;
+}
--- Series/series2.c
+++ Series/series2.c
@@ -4,15 +4,18 @@ int main() {
 
     int n1, n2, i=1, j=2, sum = 0;
 
-    printf("Enter two serial integer n1 and n2: ");
+    printf("Series 1*2 + 2*3 + 3*4 + n1 * n2.\nEnter two serial integer n1 and n2: ");
     scanf("%d %d", &n1, &n2);
 
     while (i <= n1 && j <= n2) {
         sum = sum + i * j;
+
+        i == n1 ? printf(" %d*%d", i, j) : printf(" %d*%d +", i, j);
+
         i++; j++;
     }
 
-    printf("Sum of serial multiply = %d \n", sum);
+    printf(" = %d \n", sum);
 
     return 0;
 }
