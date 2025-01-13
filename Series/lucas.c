f6d0c78780b9d8a395cf311281c6a38b0d231089
Emon <atitudemon@gmail.com>
2025-01-13 23:02:35 +0600

    lucas series added

--- Series/lucas.c
+++ Series/lucas.c
@@ -2,7 +2,20 @@
 
 int main() {
 
-    
+    int n, i, first = 2, second = 1, nextNum;
+
+    printf("Enter lucas series count 'n' = ");
+    scanf("%d", &n);
+
+    printf("%d, %d, ", first, second);
+
+    for(i=3; i<=n; i++){
+        nextNum = first + second;
+        first = second;
+        second = nextNum;
+
+        printf("%d, ", nextNum);
+    }
 
     return 0;
 }
