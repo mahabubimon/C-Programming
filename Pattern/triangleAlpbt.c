b2a9e917ee39fe04818b2dfdd3b2cb1f0e9b397c
Emon <atitudemon@gmail.com>
2025-01-14 20:24:29 +0600

    alp pattern added

--- Pattern/triangleAlpbt.c
+++ Pattern/triangleAlpbt.c
@@ -2,26 +2,27 @@
 
 int main() {
 
-    int n, row,col;
+    int n, row, col;
 
-    printf("Enter row 'n' = ");
+    printf("Enter row count 'n' = ");
     scanf("%d", &n);
 
-    printf("\nTriangle pattern of column value. \n");
+    printf("\nTriangle pattern of Upper Case.\n");
     for (row=1; row<=n; row++) {
         
         for(col=1; col<=row; col++) {
-            printf("%d ", col);
+            printf("%c ", col+64);
         }
         printf("\n");
 
     }
 
-    printf("\nTriangle pattern of row value. \n");
+    printf("\nTriangle pattern of Lower Case.\n");
     for (row=1; row<=n; row++) {
         
         for(col=1; col<=row; col++) {
-            printf("%d ", row);
+            printf("%c ", row+96
+            );
         }
         printf("\n");
     }
