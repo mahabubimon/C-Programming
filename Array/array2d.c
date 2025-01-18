751a37c8c653c1f6f5149659fce2ede985543a3d
Emon <atitudemon@gmail.com>
2025-01-18 13:05:13 +0600

    2D array added

--- Array/array2d.c
+++ Array/array2d.c
@@ -2,7 +2,26 @@
 
 int main() {
 
-    
+    int numArr[4][5] = { 
+        {2, 4, 6, 8, 10}, 
+        {1, 3, 5, 7, 9}, 
+        {3, 7, 11, 15, 19}, 
+        {2, 12, 30, 56, 90}
+    }, i, j;
+
+    // Iteration on 2D array
+    printf("2D array = { \n");
+
+    for (i=0; i<4; i++) {
+        printf("  { ");
+        for (j=0;j<4; j++) {
+            printf("%d, ", numArr[i][j]);
+        }
+         printf("%d ", numArr[i][j]);
+         printf("}, \n");
+    }
+
+    printf("} \n");
 
     return 0;
 }
