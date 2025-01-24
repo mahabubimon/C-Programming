96a65926a24a2fde5fc793b86932403366786fdc
Emon <atitudemon@gmail.com>
2025-01-24 19:02:02 +0600

    upper case func added

--- String/upperStr.c
+++ String/upperStr.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
 
-    
+    char str[30];
+
+    printf("Enter your string: ");
+    gets(str);
+
+    strupr(str);
+    // strlwr(str);
+
+    printf("Uppercase: %s\n", str);
 
     return 0;
 }
