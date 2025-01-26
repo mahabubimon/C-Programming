6df7e45bc27082cbbe26c8665083cc591c7b6de0
Emon <atitudemon@gmail.com>
2025-01-26 22:54:02 +0600

    recursive func added

--- 12. Function/recursion.c
+++ 12. Function/recursion.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 
+int factorial (int n) {
+    if (n==1) return 1;
+    else return n * factorial(n-1);
+}
+
 int main() {
 
-    
+    int n;
+
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    int result = factorial(n);
+
+    printf("Factorial of %d = %d\n", n, result);
 
     return 0;
 }
